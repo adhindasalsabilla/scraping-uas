53abaf79b845ee49d9fc818e6845c1eb20ad8385
adhindasalsabilla <126484597+adhindasalsabilla@users.noreply.github.com>
2024-06-20 11:48:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ df1 = pd.read_csv(fn1, encoding='latin1')
 
 # Menampilkan DataFrame sebagai tabel
 st.dataframe(df1)
+
+# Fungsi untuk membuat visualisasi
+def visualize_top_10(df):
+    # Ambil 10 baris pertama
+    top_10 = df.head(10)
+
+    # Ambil kolom yang relevan
+    judul_film = top_10['judul']
+    rating = top_10['rating']
+
+    # Buat visualisasi
+    plt.figure(figsize=(10, 8))
+    plt.barh(judul_film, rating, color='skyblue')
+    plt.title('Top 10 Rating Film di IMDB')
+    plt.xlabel('Rating')
+    plt.ylabel('Judul Film')
+    st.pyplot(plt)
